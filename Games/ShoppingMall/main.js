0fcdb498a52e301a08c400509ac25fbb1c76b22f
mr-yanghm <mr.yanghm@gmail.com>
2020-12-04 00:11:48 +0900

    shopping mall 연습

---
+++ Games/ShoppingMall/main.js
@@ -0,0 +1,72 @@
+'use strict';
+
+// Fetch the items from the JSON file
+function loadItems() {
+
+    return fetch('./data/data.json')
+    .then(response => response.json())
+    .then(json => json.items);
+}
+
+// Update the list with the given items
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+// Create HTML list item from the given data item
+function createHTMLString(item) {
+    return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail">
+        <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
+}
+
+function updateItem(items, key, value) {
+    const container = document.querySelectorAll('.items item');
+
+    items.forEach(item => {
+        if(item.dataset[key] !== value){
+            item.classList.add('unVisible');
+        }else{
+            item.classList.remove('unVisible');
+        }
+        
+    });
+}
+
+function onButtonClick(event, items) {
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
+
+    if(key == null || value == null){
+        return;
+    }
+
+    // updateItem(items, key, value);
+    displayItems(items.filter(item => item[key] === value));
+}
+
+// EventHandler setting
+function setEventListeners(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector(".buttons");
+
+    logo.addEventListener('click', () => {
+        displayItems(items);
+    })
+
+    buttons.addEventListener('click', event => onButtonClick(event, items));
+
+}
+
+// main
+loadItems()
+.then(items => {
+    displayItems(items);
+    setEventListeners(items);
+})
+.catch(console.log);
